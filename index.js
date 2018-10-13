f23500a5c247d047cabc0e4551756062047b4d5b
Thiago <thiago.carvp@gmail.com>
2018-10-13 17:56:42 -0300

    Add route to get logs

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000; 
 
+const logs = [];
+
+app.get('/', function (req, res) {
+  res.json(logs);
+});
+
 app.post('/ci', function (req, res) {
   console.log(req);
+  logs.push(req);
   res.json();
 });
 
