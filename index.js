21ed88dba291fe592127054fcfc7fb0cf5a4be38
Thiago <thiago.carvp@gmail.com>
2018-10-13 17:53:47 -0300

    Add post route

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; 
+
+app.post('/ci', function (req, res) {
+  console.log(req);
+  res.json();
+});
+
+app.listen(port, () => {
+  console.log('Listening on port ' + port);
+});
