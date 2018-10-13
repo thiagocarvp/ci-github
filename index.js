187440475cf85b3a1a0c40501634fd42a7b01c40
Thiago <thiago.carvp@gmail.com>
2018-10-13 18:31:04 -0300

    Add body-parser

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000; 
 
 const logs = [];
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.json(logs);
 });
 
 app.post('/ci', function (req, res) {
-  console.log(req);
-  logs.push(req);
+  console.log(req.body);
+  logs.push(req.body);
   res.json();
 });
 
-app.listen(port, () => {
+app.listen(port, function () {
   console.log('Listening on port ' + port);
 });
